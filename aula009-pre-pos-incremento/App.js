699ec0a968dd9a10ffd68d850755b974e6839fd8
Ângelo <angelobielcosta123@gmail.com>
2024-08-25 10:09:16 -0300

    pre e pos incremento

---
+++ aula009-pre-pos-incremento/App.js
@@ -0,0 +1,9 @@
+let n=-10
+let x=-n
+let n1 =10
+let n2 = 15
+
+console.log(n--) // pós incremento imprime o valor depois de executar
+console.log(--n) // pré incremento imprime o valor antes de executar
+console.log(++n)
+console.log(n1+n2)
