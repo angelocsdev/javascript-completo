b0d8ec965f7b383ced0c1e8604d479c0ad592cb5
Ângelo <angelobielcosta123@gmail.com>
2024-08-25 09:45:09 -0300

    operadores logicos

---
+++ aula007-op-logicos/App.js
@@ -0,0 +1,20 @@
+// && -> and e
+// || -> or ou
+// ! -> not não
+
+let n1, n2, n3, n4
+
+n1=10
+n2=5
+n3=70
+n4=8
+
+console.log((n1>n2)&&(n1>n3)) // false
+console.log((n1>n2)||(n1>n3)) // true
+console.log(!(n1>n2)(n1>n3)) // false
+
+if(n1>n2) {
+    console.log(n1 + " maior que " + n2)
+} else {
+    console.log(n1 + " menor que " + n2)
+}
