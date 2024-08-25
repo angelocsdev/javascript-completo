86dfca6655dab1fab7c871a156db1342291b3982
Ângelo <angelobielcosta123@gmail.com>
2024-08-25 09:09:23 -0300

    operadores matematicos

---
+++ aula004-op-matematicos/App.js
@@ -0,0 +1,30 @@
+/*
++ -> adição
+- -> subtração
+/ -> divisão
+* -> multiplicação
+% -> resto da divisão
+++ -> valor +1
+-- -> valor -1
++= -> mesma coisa que valor+valor=resultado
+-= -> mesma coisa que valor-valor=resultado
+*/
+
+let num1=0, num2=0, num3 = 0, res=0
+
+num1 = 5
+
+num1++
+
+num2 = 10
+
+num3 = 12
+
+num3--
+
+res = (num1 + num2 + num3) / 3
+
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(res)
