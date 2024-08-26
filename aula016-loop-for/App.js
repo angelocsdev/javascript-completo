cfeae6bda96c476c9d11ac1f27f83d2b31b7a14d
Ângelo <angelobielcosta123@gmail.com>
2024-08-26 16:30:40 -0300

    for in e for off

---
+++ aula016-loop-for/App.js
@@ -0,0 +1,14 @@
+let num=[10,20,30,40,50]
+
+for (let i = 0; i < num.length; i++) {
+    console.log(num[i])
+    
+}
+
+for(i in num ) {
+    console.log(num[i])
+}
+
+for(i of num) {
+    console.log(i)
+}
