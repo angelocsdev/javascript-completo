39c09cb432653503d255ea6e92627a08b6cf4d12
Ângelo <angelobielcosta123@gmail.com>
2024-08-26 16:01:32 -0300

    switch case

---
+++ aula014-swithcase/App.js
@@ -0,0 +1,16 @@
+let podio=1
+
+switch(podio) {
+    case 1:
+        console.log("1º lugar")
+        break
+    case 2:
+        console.log("2º lugar")
+        break
+    case 3:
+        console.log("3º lugar")
+        break
+    default:
+        console.log("Não subiu ao pódio!")
+        break
+}
